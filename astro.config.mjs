92a2abf174c740487ded78d2da7b0906e066e803
Lautaro Llusa <llusa.lautaro@gmail.com>
2024-05-04 14:07:01 -0300

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://talingo.github.io',
   output: "server",
   adapter: node({
     mode: "standalone"
